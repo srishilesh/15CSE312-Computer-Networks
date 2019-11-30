f4aa5b92b23c65cc4a273203b6256b302aa53fb8
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2019-11-30 18:21:38 +0530

    Create client1_file.py

---
+++ Lab 5/Single Server Multiple Clients/client1_file.py
@@ -0,0 +1,31 @@
+import socket 
+  
+  
+def main(): 
+    
+    host = '127.0.0.1'
+    port = 5000
+  
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+    s.connect((host,port)) 
+    
+    #while True: 
+    inp = input(" >> ")
+    message = "From Client 1: "+inp
+    
+    s.send(message.encode('ascii')) 
+
+    data = s.recv(1024) 
+    print('Received from the server :',str(data.decode('ascii'))) 
+
+        # ans = input('\nDo you want to continue(y/n) :') 
+        # if ans == 'y': 
+        #     continue
+        # else: 
+        #     break
+    # fin = s.recv(1024).decode()
+    # print(fin)
+    s.close() 
+  
+if __name__ == '__main__': 
+    main() 
